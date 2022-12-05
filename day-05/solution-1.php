8c9fcd912273f41a00f8e6766505fe020686507e
Denis Zyk <44240055+denis-zyk@users.noreply.github.com>
2022-12-05 14:07:50 +0200

    Add day-5 solution

---
+++ day-05/solution-1.php
@@ -0,0 +1,65 @@
+<?php
+
+$inputFile = 'input.txt';
+
+$data = file("./$inputFile", FILE_IGNORE_NEW_LINES);
+
+const MOVE = 'move ';
+const FROM = 'from ';
+const TO = 'to ';
+
+define('MOVE_OFFSET', strlen(MOVE));
+define('SRC_STACK_OFFSET', strlen(FROM));
+define('DST_STACK_OFFSET', strlen(TO));
+
+function parseInput($data) {
+    $stacks = [];
+    $moves = [];
+
+    $i = 0;
+    foreach ($data as $line) {
+        if (strstr($line, '[')) {
+            parseStacks($stacks, $line);
+        } elseif (strstr($line, 'move')) {
+            $moves[] = [
+                'count' => (int)substr($line, strpos($line, MOVE) + MOVE_OFFSET),
+                'src' => (int)substr($line, strpos($line, FROM) + SRC_STACK_OFFSET),
+                'dst' => (int)substr($line, strpos($line, TO) + DST_STACK_OFFSET),
+            ];
+        }
+        $i++;
+    }
+
+    ksort($stacks);
+    return [$stacks, $moves];
+}
+
+function parseStacks(&$stacks, $line) {
+    $stackIndex = 0;
+    for ($i = 1; $i < strlen($line); $i += 4, $stackIndex++) {
+        if ($line[$i] != ' ') {
+            if (!isset($stacks[$stackIndex])) {
+                $stacks[$stackIndex] = [];
+            }
+            array_unshift($stacks[$stackIndex], $line[$i]);
+        }
+    }
+
+    return $stacks;
+}
+
+[$stacks, $moves] = parseInput($data);
+
+foreach ($moves as $move) {
+    for ($i = 0; $i < $move['count']; $i++) {
+        $item = array_pop($stacks[$move['src'] - 1]);
+        $stacks[$move['dst'] - 1][] = $item;
+    }
+}
+
+foreach ($stacks as $stack) {
+    echo array_pop($stack);
+}
+
+echo PHP_EOL;
+
